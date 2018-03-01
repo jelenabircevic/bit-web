9ac9329c1a65d324cd4e5aedf4023e3f9c5beb3e
BIT Student <classroom@bgit.rs>
2018-03-01 18:29:26 +0100

    marion forever

--- w5/events/Mario/app.js
+++ w5/events/Mario/app.js
@@ -2,6 +2,7 @@ var mario = document.querySelector('#mario');
 var runner = document.querySelector('#running');
 var ground = document.querySelector('.container');
 var timer;
+var run = false;
 ground.style.backgroundPositionX = '100px';
 document.addEventListener('keydown', startRunning);
 document.addEventListener('keyup', stopRunning);
@@ -14,15 +15,19 @@ function freeze() {
     runner.style.display = 'none';
 }
 function startRunning(e) {
-    if (e.keyCode == 39) {
+    if (e.keyCode == 39 && !run) {
         runMarioRun();
-        timer = setInterval(groundMove(), 50);
+        timer = setInterval(groundMove, 50);
+        run = true;
+    } else {
+        return;
     }
 }
 function stopRunning(e) {
     if (e.keyCode == 39) {
         freeze();
         clearInterval(timer);
+        run = false;
     }
 }
 /* onkeydown = function(e) {
